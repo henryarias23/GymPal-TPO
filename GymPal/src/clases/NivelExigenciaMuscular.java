4e67af1a88cf748997af02ff03dde2e42871d22b
Henry Arias <73914099+henryarias23@users.noreply.github.com>
2024-11-11 14:15:12 -0300

    b

--- GymPal/src/clases/NivelExigenciaMuscular.java
+++ GymPal/src/clases/NivelExigenciaMuscular.java
@@ -1,11 +1,5 @@
 package clases;
 
-public class NivelExigenciaMuscular {
-    public Nivel nivel;
-
-    public NivelExigenciaMuscular(Nivel nivel) {
-        this.nivel = nivel;
-    }
 public enum NivelExigenciaMuscular {
     BAJO,
     MEDIO,
